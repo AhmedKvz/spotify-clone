19d0b796cfeb93a7f2d4dd2c1689b57dbffbbaf3
Kvz666 <55993619+Kvz666@users.noreply.github.com>
2022-09-27 01:28:12 +0200

    added rapid api for songs and redux toolkit reducer

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "5588c752bamsh37c3dfd0bfc0dc8p1980bejsn77f3d6701b35"
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
