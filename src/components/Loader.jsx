8206dacee021f76fc0f9c34769cff7e9b0e986ae
Kvz666 <55993619+Kvz666@users.noreply.github.com>
2022-09-28 18:43:43 +0200

    added loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,3 +1,10 @@
-const Loader = () => <div>Loader</div>;
+import { loader } from "../assets";
+
+const Loader = ({ title }) => (
+  <div className="w-full flex justify-center items-center flex-col">
+    <h1>{title}</h1>
+    <img src={loader} alt="loading..." />
+  </div>
+);
 
 export default Loader;
